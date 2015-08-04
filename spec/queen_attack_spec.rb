3c21ee762ae6e965438d2de9f37bdd86bb91165c
Alexander S. Dickinson <alexandersdickinson@gmail.com>
2015-08-03 21:59:49 -0700

    Pass second spec.

--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -5,4 +5,8 @@ describe('Array#queen_attack?') do
   it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other') do
     expect([1,1].queen_attack?([2, 3])).to(eq(false))
   end
+  
+  it('is true when x coordinates match') do
+    expect([1,9].queen_attack?([1,2])).to(eq(true))
+  end
 end
