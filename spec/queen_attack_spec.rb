c27e677f7d9b0ee26745b5aa7d41bceaa6e8561f
Alexander S. Dickinson <alexandersdickinson@gmail.com>
2015-08-03 22:01:12 -0700

    Pass third spec

--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -9,4 +9,8 @@ describe('Array#queen_attack?') do
   it('is true when x coordinates match') do
     expect([1,9].queen_attack?([1,2])).to(eq(true))
   end
+  
+  it('is true when y coordinates match') do
+    expect([1,3].queen_attack?([9,3])).to(eq(true))
+  end
 end
